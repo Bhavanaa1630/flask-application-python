938405ac5c1cf03143bf89c4dd2ee6c44407bcc9
Bhavanaa1630 <148368033+Bhavanaa1630@users.noreply.github.com>
2024-06-28 17:18:18 +0530

    Update license_checker.js

--- license_checker.js
+++ license_checker.js
@@ -1,15 +1,6 @@
 const licenseChecker = require('license-checker');
 const fs = require('fs');
 
-// Read package.json to get dependencies
-const packageJson = require('./package.json');
-
-// Convert dependencies to array format expected by license-checker
-const dependencies = Object.keys(packageJson.dependencies).map(pkg => `${pkg}@${packageJson.dependencies[pkg]}`);
-
-// Read licenses.txt to get list of allowed licenses
-const allowedLicenses = fs.readFileSync('licenses.txt', 'utf8').split('\n').map(line => line.trim()).filter(line => line !== '');
-
 // Options for license-checker
 const options = {
   start: './',
@@ -17,8 +8,7 @@ const options = {
   json: true,
   unknown: true,
   onlyAllow: true,
-  onlyDirectDependencies: true,
-  filter: allowedLicenses.join(',')
+  onlyDirectDependencies: true
 };
 
 // Run license-checker
@@ -28,7 +18,7 @@ licenseChecker.init(options, function (err, json) {
     return;
   }
 
-  // Write filtered license information to a JSON file
+  // Write license information to a JSON file
   fs.writeFileSync('licenses.json', JSON.stringify(json, null, 2));
-  console.log('Filtered license information saved to licenses.json');
+  console.log('License information saved to licenses.json');
 });
